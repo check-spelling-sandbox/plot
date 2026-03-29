0c612cd5591b0fff5b5e9bb0f56b96ef34e7f72f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-03-29 11:18:45 -0400

    spelling: shouldn't
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- test/plots/autoplot.ts
+++ test/plots/autoplot.ts
@@ -126,7 +126,7 @@ export async function autoConnectedScatterplot() {
   return Plot.auto(driving, {x: "miles", y: "gas", mark: "line"}).plot();
 }
 
-// shouldnt make a line
+// shouldn't make a line
 export async function autoDotUnsortedDate() {
   const athletes = await d3.csv<any>("data/athletes.csv", d3.autoType);
   return Plot.auto(athletes, {x: "date_of_birth", y: "height"}).plot();
